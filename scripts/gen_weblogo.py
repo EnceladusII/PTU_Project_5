2252e1d6372aa1d1584562a9c5f09f0b2c4c9278
Encelade <wittenmeyerguillaume@proton.me>
2024-11-21 13:07:43 +0100

    conda env update

--- scripts/gen_weblogo.py
+++ scripts/gen_weblogo.py
@@ -1,12 +1,13 @@
 from weblogo import *
 from pathlib import Path
+import os
 
 # Path utils:
 commonpath = Path(__file__).parent.parent
-rawdata_folder = commonpath / 'data' / '1_intermediate' / 'MAFFT_alignment'
+rawdata_folder = commonpath / 'data' / '1_intermediate' / 'MAFFT_alignment' / 'occupancy'
 
 # Charger les séquences depuis un fichier FASTA
-fin = open(rawdata_folder / 'MAFFT_WD_sequence.fasta')
+fin = open(rawdata_folder / 'MAFFT_WD_sequence_fil_occ_0.7.fasta')
 seqs = read_seq_data(fin)
 
 # Créer l'objet LogoData
@@ -23,9 +24,10 @@ logoformat = LogoFormat(logodata, logooptions)
 png_data = png_formatter(logodata, logoformat)
 
 # Sauvegarder le logo en fichier SVG
-output_path = commonpath / 'output_logo.svg'
+output_path = commonpath / 'output_logo.png'
 with open(output_path, 'wb') as f:
     f.write(png_data)
     
 print(f"Logo SVG généré et sauvegardé sous {output_path}")
 
+os.system(f"weblogo  < {rawdata_folder / 'MAFFT_WD_sequence_fil_occ_0.7.fasta'} > {commonpath / 'test2.png --format png --scale-width yes'}")
